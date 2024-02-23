a04fe79f2061ec8ca4f7ac0ca7081a265a7ebea1
Zakhar Tsidvintsev <ibudenov2@gmail.com>
2024-02-23 21:05:23 +0500

    remove old project

--- Lab-Work-5/code/api/Models/Event.cs
+++
@@ -1,7 +0,0 @@
-namespace api.Models;
-
-public class Event
-{
-    public Guid EventId { get; init; } = Guid.NewGuid();
-    public string EventName { get; set; } = string.Empty;
-}
--- Lab-Work-5/code/api/Models/Ticket.cs
+++
@@ -1,8 +0,0 @@
-namespace api.Models;
-
-public class Ticket
-{
-    public Guid TicketId { get; init; } = Guid.NewGuid();
-    public Guid EventId { get; init; }
-    public string Owner { get; set; } = string.Empty;
-}
