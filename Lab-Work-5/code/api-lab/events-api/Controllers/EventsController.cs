f500d3ef20809fbf1ecdacede0fa22df3acc375f
Zakhar Tsidvintsev <ibudenov2@gmail.com>
2024-02-28 22:49:00 +0500

    fake error

--- Lab-Work-5/code/api-lab/events-api/Controllers/EventsController.cs
+++ Lab-Work-5/code/api-lab/events-api/Controllers/EventsController.cs
@@ -30,7 +30,7 @@ public class EventsController(IEventsRepository eventsRepository) : ControllerBa
             var newEvent = new Event() { EventName = eventName };
             await eventsRepository.AddAsync(newEvent);
 
-            return Ok();
+            return StatusCode(StatusCodes.Status500InternalServerError);
         }
         catch (Exception e)
         {
